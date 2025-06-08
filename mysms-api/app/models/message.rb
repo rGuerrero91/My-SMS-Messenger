8a70edab36ac920859b87437d3f9bb4075cb3c34
Rudy Guerrero <r.guerreronyc@gmail.com>
2025-06-08 06:49:37 -0400

    Update message.rb

--- mysms-api/app/models/message.rb
+++ mysms-api/app/models/message.rb
@@ -4,9 +4,9 @@ class Message
   field :user_id, type: BSON::ObjectId
   field :body, type: String
   field :to, type: String
-  field :session_id, type: String
   field :twilio_sid, type: String, default: :null
   field :status, type: String
+  # field :session_id, type: String
 
   belongs_to :user
 end
