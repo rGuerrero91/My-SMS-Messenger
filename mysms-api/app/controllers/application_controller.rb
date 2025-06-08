2fcbadad676bcf162c81f134ba7e3db64dba104a
Rudy Guerrero <r.guerreronyc@gmail.com>
2025-06-08 04:57:10 -0400

    render devise controller responses in a format that API can handle

--- mysms-api/app/controllers/application_controller.rb
+++ mysms-api/app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::API
-
-    # had to include this, because devise loses its marbles in api only mode.
+    # had to include this because devise loses its marbles in api only mode.
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
 
-      protected
+    rescue_from Devise::MissingWarden do
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  
+    # This is the key override for API-style unauthorized responses
+    def authenticate_user!(opts = {})
+        unless user_signed_in?
+            render json: { error: 'Please sign in before continuing.' }, status: :unauthorized
+        end
+    end 
+    protected
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
